d3bf77513c727e3b2e3b39e262e2089b95704d38
BorisiukIvan <121752518+BorisiukIvan@users.noreply.github.com>
2023-10-26 20:06:04 +0200

    Update and rename eratosfen.py to DivisorsTheory/eratosfen.py

--- DivisorsTheory/eratosfen.py
+++ DivisorsTheory/eratosfen.py
@@ -1,13 +1,14 @@
 #coding: utf-8
+# get all prime numbers up to 1000
 
 x = 1000
 arr = [0, 0] + [1] * (x - 2)
 #prime = []
-print 'Массив готов!'
+print('Массив готов!')
 
 s = 1
 for i in range(x):
     if (not arr[i]): continue
-    print i
+    print(i)
     for j in range(i*i, x, i): arr[j] = 0
 
